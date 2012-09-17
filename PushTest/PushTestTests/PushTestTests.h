0bbc35dc2b86961363d32740580176b439c43eb0
ikjy05 <iksk1279@paran.com>
2012-09-17 16:02:56 +0900

    푸쉬서버
    연구중

---
+++ PushTest/PushTestTests/PushTestTests.h
@@ -0,0 +1,13 @@
+//
+//  PushTestTests.h
+//  PushTestTests
+//
+//  Created by JEONG YOUN KIM on 12. 9. 17..
+//  Copyright (c) 2012년 JEONG YOUN KIM. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PushTestTests : SenTestCase
+
+@end
---
+++ PushTest/PushTestTests/PushTestTests.m
@@ -0,0 +1,32 @@
+//
+//  PushTestTests.m
+//  PushTestTests
+//
+//  Created by JEONG YOUN KIM on 12. 9. 17..
+//  Copyright (c) 2012년 JEONG YOUN KIM. All rights reserved.
+//
+
+#import "PushTestTests.h"
+
+@implementation PushTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PushTestTests");
+}
+
+@end
